02fd2ca1f15d79fd6f2e1fca1c666609ab4834f9
MarianaMCB <marianam.correalb@utadeo.edu.co>
2022-11-19 16:00:13 -0500

    Conectar Repositorio GitHub y Azure

--- App.js
+++ App.js
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginVertical: 10,
     borderColor: '#37462C',
-    borderLeftWidth: 6,
+    borderLeftWidth: 7,
     borderBottomWidth: 7,
   },
 });
